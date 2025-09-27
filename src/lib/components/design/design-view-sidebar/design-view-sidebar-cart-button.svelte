38cd51673c5a3094d6b3fd3055290956ad7299fd
uidumajogu <idumajogu@gmail.com>
2025-09-27 05:34:57 -0400

    ✨Feat(cart): Display cart item details and total in sidebar button

--- src/lib/components/design/design-view-sidebar/design-view-sidebar-cart-button.svelte
+++ src/lib/components/design/design-view-sidebar/design-view-sidebar-cart-button.svelte
@@ -5,6 +5,7 @@
 	import CartPreviewIcon from '$lib/components/cart-preview-icon.svelte';
 	import { goto } from '$app/navigation';
 	import { page } from '$app/state';
+	import number from '$lib/utils/number';
 
 	const workspaceId = page.data.activeWorkspaceId;
 
@@ -14,6 +15,12 @@
 
 	const hasCartItems = $derived(cart.cartItems.length > 0);
 
+	const totalCart = $derived.by(() =>
+		cart.cartItems
+			.map((cartItem) => cartItem.product.retailPrice * (cartItem.quantity ?? 0))
+			.reduce((a, b) => a + b, 0)
+	);
+
 	function onclick() {
 		if (!hasCartItems) saveCartItems(productIds.map((productId) => ({ productId, quantity: 1 })));
 
@@ -21,14 +28,47 @@
 	}
 </script>
 
-<Button class="gap-x-4" {onclick}>
-	<CartPreviewIcon />
+<div class="w-full">
+	<Button class="gap-x-4" {onclick}>
+		<CartPreviewIcon />
 
-	{#if !hasCartItems}
-		<span>Add all to Cart</span>
-	{/if}
+		{#if !hasCartItems}
+			<span>Add all to Cart</span>
+		{/if}
+
+		{#if hasCartItems}
+			<span>View Cart</span>
+		{/if}
+	</Button>
 
 	{#if hasCartItems}
-		<span>View Cart</span>
+		<div
+			class="scrollbar-thin h-[50vh] w-full overflow-y-auto rounded-md bg-color-background-surface p-8 pb-96"
+		>
+			{#each cart.cartItems as cartItem (cartItem._id)}
+				<div
+					class="flex h-10 w-full items-center gap-x-2 border-t border-color-border text-sm text-color-text-muted"
+				>
+					<div class="flex w-full gap-x-2">
+						<p class="line-clamp-1 w-5/6">{cartItem.product.name}</p>
+						<p class="w-1/6 text-center">{cartItem.quantity}</p>
+					</div>
+
+					<h4 class="w-20 text-end font-medium">
+						{number.toMoney(cartItem.product.retailPrice, cart.currencyCode)}
+					</h4>
+				</div>
+			{/each}
+
+			<div
+				class="flex h-10 w-full items-center justify-between gap-x-2 border-t border-color-border text-sm font-semibold"
+			>
+				<p>Total</p>
+
+				<h4 class="text-end">
+					{number.toMoney(totalCart, cart.currencyCode)}
+				</h4>
+			</div>
+		</div>
 	{/if}
-</Button>
+</div>
