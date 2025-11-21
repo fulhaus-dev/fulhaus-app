4caa189f50e70ab2ebb1c24f042f8b090a464da4
uidumajogu <idumajogu@gmail.com>
2025-11-21 15:24:15 -0500

    feat(pinterest): store and validate user ID with access token
    Introduces a change to how the Pinterest user token is stored and validated. The user ID is now stored alongside the access token in the cookie, separated by a delimiter. This allows for validation on subsequent requests to ensure the token belongs to the currently authenticated user.
    
    Additionally, a new cookie `APP_CURRENT_USER_ID_COOKIE_NAME` is introduced to store the current user's ID. This change aims to improve security and prevent token misuse by ensuring that the token being used is associated with the correct user.

--- src/routes/api/pinterest/pins/+server.ts
+++ src/routes/api/pinterest/pins/+server.ts
@@ -1,4 +1,8 @@
-import { PINTEREST_USER_TOKEN_COOKIE_NAME } from '$env/static/private';
+import {
+	APP_CURRENT_USER_ID_COOKIE_NAME,
+	PINTEREST_USER_TOKEN_COOKIE_DELIMITER,
+	PINTEREST_USER_TOKEN_COOKIE_NAME
+} from '$env/static/private';
 import type { ImageData, PinterestPinItem } from '$lib/types';
 import { error } from '@sveltejs/kit';
 
@@ -8,13 +12,23 @@ export async function GET({ url, cookies }) {
 	const userToken = cookies.get(PINTEREST_USER_TOKEN_COOKIE_NAME);
 	if (!userToken) throw error(401, 'Unauthorized');
 
+	const userId = cookies.get(APP_CURRENT_USER_ID_COOKIE_NAME);
+	if (!userId) throw error(401, 'Unauthorized');
+
+	const userTokenSplit = userToken.split(PINTEREST_USER_TOKEN_COOKIE_DELIMITER);
+
+	const tokenUserId = userTokenSplit[0];
+	if (tokenUserId !== userId) throw error(401, 'Unauthorized');
+
+	const token = userTokenSplit[1];
+
 	const bookmark = url.searchParams.get('bookmark');
 	const endpoint = bookmark
 		? `https://api.pinterest.com/v5/pins?bookmark=${bookmark}&page_size=${PAGINATION_SIZE}`
 		: 'https://api.pinterest.com/v5/pins?page_size=50';
 
 	const res = await fetch(endpoint, {
-		headers: { Authorization: `Bearer ${userToken}` }
+		headers: { Authorization: `Bearer ${token}` }
 	});
 
 	const data = await res.json();
