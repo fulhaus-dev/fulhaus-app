44ee69fe8a67678048e08b1d88592fc9d21cee43
uidumajogu <idumajogu@gmail.com>
2025-12-01 15:52:24 -0500

    ✨Feat(posthog): Integrate PostHog for analytics

---
+++ src/routes/+layout.ts
@@ -0,0 +1,16 @@
+import posthog from 'posthog-js';
+import { browser } from '$app/environment';
+import { PUBLIC_POSTHOG_KEY, PUBLIC_POSTHOG_HOST } from '$env/static/public';
+
+export const load = async () => {
+	if (browser) {
+		posthog.init(PUBLIC_POSTHOG_KEY, {
+			api_host: PUBLIC_POSTHOG_HOST,
+			capture_pageview: true,
+			capture_pageleave: true,
+			capture_exceptions: true,
+			defaults: '2025-11-30'
+		});
+	}
+	return;
+};
